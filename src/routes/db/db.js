34c1a353060ef10c970aa9ce083c70da31d69453
Lucio Marchesi <marchesilucio14@gmail.com>
2024-06-22 15:00:09 -0300

    conección anda

--- src/routes/db/db.js
+++ src/routes/db/db.js
@@ -3,7 +3,7 @@ const mySql = require("mysql2");
 const connection = mySql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Loveisall77",
+  password: "root",
   database: "usuarios_db",
 });
 
