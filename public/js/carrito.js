a6573d5ca2693b25e60c2dd6733f6df9d9bd7e94
Lucio Marchesi <marchesilucio14@gmail.com>
2024-07-01 01:00:53 -0300

    Carrito de compras

---
+++ public/js/carrito.js
@@ -0,0 +1,112 @@
+let openShopping = document.querySelector(".shopping");
+let closeShopping = document.querySelector(".closeShopping");
+let list = document.querySelector(".list");
+let listCard = document.querySelector(".listCard");
+let body = document.querySelector("body");
+let total = document.querySelector(".total");
+let quantity = document.querySelector(".quantity");
+
+openShopping.addEventListener("click", () => {
+  body.classList.add("active");
+});
+closeShopping.addEventListener("click", () => {
+  body.classList.remove("active");
+});
+
+let products = [
+  {
+    id: 1,
+    name: "Tazón de Pollo y Verduras Frescas",
+    image: "1.PNG",
+    price: 20000,
+  },
+  {
+    id: 2,
+    name: "Pollo a la Parrilla con Salsa BBQ",
+    image: "2.PNG",
+    price: 24000,
+  },
+  {
+    id: 3,
+    name: "Ensalada de Salmón Ahumado",
+    image: "3.PNG",
+    price: 25000,
+  },
+  {
+    id: 4,
+    name: "Crema de Calabaza",
+    image: "4.PNG",
+    price: 23000,
+  },
+  {
+    id: 5,
+    name: "Ensalada Verde Primavera",
+    image: "5.PNG",
+    price: 16000,
+  },
+  {
+    id: 6,
+    name: "Pizza Margarita Clásica",
+    image: "6.PNG",
+    price: 12000,
+  },
+];
+let listCards = [];
+function initApp() {
+  products.forEach((value, key) => {
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("item");
+    newDiv.innerHTML = `
+            <img src="../assets/img/${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCard(${key})">Agregar al Carrito</button>`;
+    list.appendChild(newDiv);
+  });
+}
+initApp();
+function addToCard(key) {
+  if (listCards[key] == null) {
+    // copy product form list to list card
+    listCards[key] = JSON.parse(JSON.stringify(products[key]));
+    listCards[key].quantity = 1;
+  }
+  reloadCard();
+}
+function reloadCard() {
+  listCard.innerHTML = "";
+  let count = 0;
+  let totalPrice = 0;
+  listCards.forEach((value, key) => {
+    totalPrice = totalPrice + value.price;
+    count = count + value.quantity;
+    if (value != null) {
+      let newDiv = document.createElement("li");
+      newDiv.innerHTML = `
+                <div><img src="../assets/img/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${
+        value.quantity - 1
+      })">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${
+        value.quantity + 1
+      })">+</button>
+                </div>`;
+      listCard.appendChild(newDiv);
+    }
+  });
+  total.innerText = totalPrice.toLocaleString();
+  quantity.innerText = count;
+}
+function changeQuantity(key, quantity) {
+  if (quantity == 0) {
+    delete listCards[key];
+  } else {
+    listCards[key].quantity = quantity;
+    listCards[key].price = quantity * products[key].price;
+  }
+  reloadCard();
+}
