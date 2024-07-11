8942977489dfaf0519de1567b093b090bcc10f1e
jlg777 <proyectojlg777@gmail.com>
2024-07-11 11:13:23 -0300

    SE CONFIGURA EL FRONT DELETE USUARIO

--- public/assets/mock/usuario.js
+++ public/assets/mock/usuario.js
@@ -1,6 +1,6 @@
 const user = {
   email: "prueba@gmail.com",
   pass: "Abc123456*",
-  email: "prueba2@gmail.com",
+  email: "jorge@prueba.com",
   pass: "Abc123456*",
 };
--- public/js/user.js
+++ public/js/user.js
@@ -1,8 +1,8 @@
 const userlogueado = JSON.parse(localStorage.getItem("user"));
 let userLogedi = document.getElementById("dropdown");
 const userNameID = document.getElementById("userName");
-const userCardTitle = document.querySelector(".card-title");
-const userCardCorreo = document.querySelector(".correo");
+const userCardTitle = document.getElementById("card-title");
+const userCardCorreo = document.getElementById("usuarioCorreo");
 const userCardName = document.querySelector(".name");
 const userCardCreateAt = document.querySelector(".createAt");
 const userCardUpdateAt = document.querySelector(".updateAt");
@@ -17,15 +17,12 @@ if (localStorage.getItem("user")) {
 
   userCardTitle.textContent = "Bienvenido";
   userCardCorreo.textContent = `Correo electrónico: ${userlogueado.userWithoutPassword.correo}`;
-  userCardName.textContent = `Nombre: ${userlogueado.userWithoutPassword.nombre}`;
-  userCardCreateAt.textContent = `Creado: ${userlogueado.userWithoutPassword.createAt}`;
-  userCardUpdateAt.textContent = `Actualizado: ${userlogueado.userWithoutPassword.updateAt}`;
-  userCardUserId.textContent = `ID: ${userlogueado.userWithoutPassword.id}`;
 }
 
 document.getElementById("btnEliminar").addEventListener("click", async () => {
   const userId = userlogueado.userWithoutPassword.id;
-  try {
+  console.log(userId);
+  /*try {
     const response = await fetch(
       `http://localhost:3000/api/usuarios/${userId}`,
       {
@@ -44,5 +41,5 @@ document.getElementById("btnEliminar").addEventListener("click", async () => {
   } catch (error) {
     console.error("Error al eliminar el usuario:", error);
     alert("Ocurrió un error al eliminar el usuario");
-  }
+  }*/
 });
