61b1af4538cb53247ccc78a94696ccab2aa3a2a3
jlg777 <proyectojlg777@gmail.com>
2024-07-11 16:41:37 -0300

    boton editar completado!

--- public/js/user.js
+++ public/js/user.js
@@ -15,55 +15,58 @@ document.addEventListener("DOMContentLoaded", () => {
     userCardTitle.textContent = "Bienvenido";
     userCardCorreo.textContent = `Correo electrónico: ${userlogueado.userWithoutPassword.correo}`;
     var userId = userlogueado.userWithoutPassword.id;
-  }
 
-  document.getElementById("btnEliminar").addEventListener("click", async () => {
-    try {
-      const response = await fetch(`https://backend-mysql-express.vercel.app/usuarios/${userId}`, {
-        method: "DELETE",
-      });
+    document.getElementById("btnEliminar").addEventListener("click", async () => {
+      try {
+        const response = await fetch(
+          `https://backend-mysql-express.vercel.app/usuarios/${userId}`,
+          {
+            method: "DELETE",
+          }
+        );
 
-      if (response.ok) {
-        alert("Usuario eliminado exitosamente");
+        if (response.ok) {
+          alert("Usuario eliminado exitosamente");
 
-        localStorage.removeItem("user");
-        window.location.href = "../index.html";
-      } else {
-        const errorText = await response.text();
-        console.log(errorText);
-        alert(`Error al eliminar el usuario: ${errorText}`);
+          localStorage.removeItem("user");
+          window.location.href = "../index.html";
+        } else {
+          const errorText = await response.text();
+          console.log(errorText);
+          alert(`Error al eliminar el usuario: ${errorText}`);
+        }
+      } catch (error) {
+        console.error("Error al eliminar el usuario:", error);
+        alert("Ocurrió un error al eliminar el usuario");
       }
-    } catch (error) {
-      console.error("Error al eliminar el usuario:", error);
-      alert("Ocurrió un error al eliminar el usuario");
-    }
-  });
+    });
 
-  document.getElementById("updateForm").addEventListener("submit", function (event) {
-    event.preventDefault();
+    document.getElementById("updateForm").addEventListener("submit", function (event) {
+      event.preventDefault();
 
-    const nuevoCorreo = document.getElementById("nuevo_correo").value;
-    const nuevaPass = document.getElementById("nuevo_pass").value;
+      const nuevoCorreo = document.getElementById("nuevo_correo").value;
+      const nuevaPass = document.getElementById("nuevo_pass").value;
 
-    // Configura los datos para la petición PUT
-    fetch(`https://backend-mysql-express.vercel.app/usuarios/${userId}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        correo: nuevoCorreo,
-        password: nuevaPass,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Éxito:", data);
-        alert("Datos actualizados exitosamente");
+      // Configura los datos para la petición PUT
+      fetch(`https://backend-mysql-express.vercel.app/usuarios/${userId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          correo: nuevoCorreo,
+          password: nuevaPass,
+        }),
       })
-      .catch((error) => {
-        console.error("Error:", error);
-        alert("Hubo un error al actualizar los datos");
-      });
-  });
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Éxito:", data);
+          alert("Datos actualizados exitosamente");
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          alert("Hubo un error al actualizar los datos");
+        });
+    });
+  }
 });
